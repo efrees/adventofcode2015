40e9b768722f02d52ec70be6156631fd79dd8f93
efrees <ericfrees@gmail.com>
2015-12-01 18:48:41 -0600

    Successful Day 1 solution, parts 1 and 2

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day1
+    {
+        public static FileInfo GetInputFileInfo()
+        {
+            return new FileInfo("..\\..\\Inputs\\Day1input.txt");
+        }
+
+        public static void ProcessInput(StreamReader inputReader)
+        {
+            var floor = 0;
+            int? firstTimeInBasement = null;
+
+            var input = inputReader.ReadLine();
+
+            var charNumber = 0;
+            foreach(var ch in input)
+            {
+                charNumber++;
+
+                if (ch == '(')
+                {
+                    floor++;
+                }
+                else if (ch == ')')
+                {
+                    floor--;
+                }
+                else
+                {
+                    //meaningless character
+                }
+
+                if (floor == -1 && !firstTimeInBasement.HasValue)
+                {
+                    firstTimeInBasement = charNumber;
+                }
+            }
+
+            Console.WriteLine("End on floor: " + floor);
+            Console.WriteLine("First time in basement: " + firstTimeInBasement);
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileInfo = Day1.GetInputFileInfo();
+
+            if (File.Exists(fileInfo.FullName))
+            {
+                using (var fileStream = fileInfo.OpenRead())
+                {
+                    using (var fileReader = new StreamReader(fileStream))
+                    {
+                        Day1.ProcessInput(fileReader);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+            }
+                Console.ReadKey();
+        }
+    }
+}
