8d79a94df692b839ab3414dc34a4ef3779b37611
Eric Frees <efrees@atlasrfid.com>
2017-02-25 15:53:37 -0600

    Day 7, part 2 solved.

--- AdventOfCode2016/Solvers/Day7Solver.cs
+++ AdventOfCode2016/Solvers/Day7Solver.cs
@@ -1,32 +1,114 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2016.Solvers
 {
     internal class Day7Solver
     {
-        public static Day7Solver Create()
+        public static Day7Solver CreateForPart1()
         {
-            return new Day7Solver();
+            return new Day7Solver
+            {
+                DesiredProtocol = "TLS"
+            };
+        }
+
+        public static Day7Solver CreateForPart2()
+        {
+            return new Day7Solver
+            {
+                DesiredProtocol = "SSL"
+            };
         }
 
         public int GetSolution(string fileText)
         {
             return fileText.SplitIntoLines()
-                .Count(line => ContainsAbba(line) && DoesNotHaveAbbaInBrackets(line));
+                .Count(SupportsDesiredProtocol);
+        }
+
+        public string DesiredProtocol { get; private set; }
+
+        private bool SupportsDesiredProtocol(string line)
+        {
+            if (DesiredProtocol == "TLS")
+                return SupportsTls(line);
+
+            return SupportsSsl(line);
+        }
+
+        private bool SupportsTls(string line)
+        {
+            return ContainsAbba(line) && DoesNotHaveAbbaInBrackets(line);
+        }
+
+        private bool SupportsSsl(string line)
+        {
+            var abaCandidates = GetAllUnBrackettedAbas(line);
+            var babCandidates = GetAllBrackettedBabs(line);
+
+            return babCandidates.Select(MatchingAbaFromBab)
+                .Intersect(abaCandidates)
+                .Any();
+        }
+
+        private string MatchingAbaFromBab(string arg)
+        {
+            return new string(new[] { arg[1], arg[0], arg[1] });
+        }
+
+        private IEnumerable<string> GetAllBrackettedBabs(string line)
+        {
+            return GetAllBrackettedSegments(line)
+                .SelectMany(GetAbasInSegment);
+        }
+
+        private IEnumerable<string> GetAllUnBrackettedAbas(string line)
+        {
+            return GetAllUnBrackettedSegments(line)
+                .SelectMany(GetAbasInSegment);
+        }
+
+        private IEnumerable<string> GetAbasInSegment(string unbrackettedSegment)
+        {
+            for (int i = 0; i < unbrackettedSegment.Length - 2; i++)
+            {
+                if (unbrackettedSegment[i] == unbrackettedSegment[i + 2]
+                    && unbrackettedSegment[i] != unbrackettedSegment[i + 1])
+                {
+                    yield return unbrackettedSegment.Substring(i, 3);
+                }
+            }
         }
 
         private bool DoesNotHaveAbbaInBrackets(string line)
         {
-            foreach (var segment in line.Split('['))
+            return !GetAllBrackettedSegments(line).Any(ContainsAbba);
+        }
+
+        private IEnumerable<string> GetAllBrackettedSegments(string line)
+        {
+            foreach (var untrimmedSegment in line.Split('[').Skip(1))
             {
-                var closeOfSegment = segment.IndexOf(']');
-                if (closeOfSegment > 0 && ContainsAbba(segment.Substring(0, closeOfSegment)))
+                var closeOfSegment = untrimmedSegment.IndexOf(']');
+                if (closeOfSegment > 0)
                 {
-                    return false;
+                    var trimmedSegment = untrimmedSegment.Substring(0, closeOfSegment);
+                    yield return trimmedSegment;
                 }
             }
-            return true;
+        }
+
+        private IEnumerable<string> GetAllUnBrackettedSegments(string line)
+        {
+            foreach (var untrimmedSegment in line.Split('['))
+            {
+                var beginningOfSegment = untrimmedSegment.IndexOf(']');
+                var trimmedSegment = untrimmedSegment.Substring(Math.Max(beginningOfSegment, 0));
+                yield return trimmedSegment;
+            }
         }
 
         private bool ContainsAbba(string line)
--- AdventOfCodeTests/Solvers/Day7Solver_should_.cs
+++ AdventOfCodeTests/Solvers/Day7Solver_should_.cs
@@ -10,9 +10,9 @@ namespace AdventOfCodeTests.Solvers
         [TestCase("asdffds")]
         [TestCase("abba[mnop]qrst")]
         [TestCase("ioxxoj[asdfgh]zxcvbn")]
-        public void count_line_if_single_line_contains_an_abba_sequence(string inputLine)
+        public void count_line_if_single_line_contains_an_abba_sequence_in_part_1(string inputLine)
         {
-            var actualResult = Day7Solver.Create().GetSolution(inputLine);
+            var actualResult = Day7Solver.CreateForPart1().GetSolution(inputLine);
             Assert.AreEqual(1, actualResult);
         }
 
@@ -20,9 +20,9 @@ namespace AdventOfCodeTests.Solvers
         [TestCase("baad")]
         [TestCase("aaaa[qwer]tyui")]
         [TestCase("notcounted")]
-        public void not_count_line_if_line_does_not_contain_an_abba_sequence(string inputLine)
+        public void not_count_line_if_line_does_not_contain_an_abba_sequence_in_part_1(string inputLine)
         {
-            var actualResult = Day7Solver.Create().GetSolution(inputLine);
+            var actualResult = Day7Solver.CreateForPart1().GetSolution(inputLine);
             Assert.AreEqual(0, actualResult);
         }
 
@@ -31,16 +31,28 @@ namespace AdventOfCodeTests.Solvers
         {
             var inputText = "abba\ndood\nnotcounted\nabba[mnop]qrst";
             var expectedCount = 3;
-            var actualResult = Day7Solver.Create().GetSolution(inputText);
+            var actualResult = Day7Solver.CreateForPart1().GetSolution(inputText);
             Assert.AreEqual(expectedCount, actualResult);
         }
 
         [TestCase("[abba]")]
         [TestCase("abcd[bddb]xyyx")]
-        public void not_count_string_with_abba_sequence_if_abba_sequence_in_brackets(string inputLine)
+        public void not_count_string_with_abba_sequence_if_abba_sequence_in_brackets_in_part_1(string inputLine)
         {
-            var actualResult = Day7Solver.Create().GetSolution(inputLine);
+            var actualResult = Day7Solver.CreateForPart1().GetSolution(inputLine);
             Assert.AreEqual(0, actualResult);
+        }
+
+        [TestCase("aba[bab]xyz", 1)]
+        [TestCase("xyx[xyx]xyx", 0)]
+        [TestCase("aaa[kek]eke", 1)]
+        [TestCase("zazbz[bzb]cdb", 1)]
+        [TestCase("abc[dedede]fg", 0)]
+        [TestCase("aaa[aaa]aaa", 0)]
+        public void look_for_corresponding_aba_and_bab_in_part_2(string inputLine, int expectedCount)
+        {
+            var actualResult = Day7Solver.CreateForPart2().GetSolution(inputLine);
+            Assert.AreEqual(expectedCount, actualResult);
         }
     }
 }
